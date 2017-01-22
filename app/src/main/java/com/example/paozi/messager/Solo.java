a362ca6a0fdb35deb7c600732944a7f5eed66970
mophsic <1084549765@qq.com>
2017-01-22 17:38:24 +0800

    jacoco 单元测试

---
+++ app/src/main/java/com/example/paozi/messager/Solo.java
@@ -0,0 +1,24 @@
+package com.example.paozi.messager;
+
+/**
+ * 作者：xiaofei
+ * 日期：2017/1/22
+ */
+
+public class Solo {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
