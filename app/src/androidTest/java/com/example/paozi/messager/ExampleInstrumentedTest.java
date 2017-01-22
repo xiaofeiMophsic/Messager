87cab15c71e3cf45bfa7a07dd0b8b792953938b8
mophsic <1084549765@qq.com>
2017-01-22 17:36:14 +0800

    jacoco 单元测试

--- app/src/androidTest/java/com/example/paozi/messager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/paozi/messager/ExampleInstrumentedTest.java
@@ -3,11 +3,10 @@ package com.example.paozi.messager;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -20,7 +19,8 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.paozi.messager", appContext.getPackageName());
+        Solo solo = new Solo();
+        solo.setId("com.android.test");
+        Log.d("TAG", solo.getId());
     }
 }
